ec9c469a8a4e69341b3b10dabe4c7101709baf1e
CORPLAN\MaindarkarA <amol.maindarkar@informa.com>
2026-02-10 14:15:36 +0000

    updated comment

--- Check Windows 11 Veriosn.ps1
+++ Check Windows 11 Veriosn.ps1
@@ -1,14 +1,14 @@
-﻿# Get OS version and build info
+﻿# Get OS version and build info.
 $OSVersion = (Get-CimInstance Win32_OperatingSystem).Version
 $BuildNumber = [int](Get-CimInstance Win32_OperatingSystem).BuildNumber
 
-# Windows 11 24H2 is expected to be build 26100 or higher
+# Windows 11 24H2 is expected to be build 26100 or higher.
 $TargetBuild = 26100
 
 Write-Host "Detected OS Version: $OSVersion"
 Write-Host "Detected Build Number: $BuildNumber"
 
-# Find OS veriosn and compare to target build.
+# Find OS a veriosn and compare to target build.
 if ($BuildNumber -lt $TargetBuild) {
     Write-Host "This system is running Windows 11 version lower than 24H2."
 } elseif ($BuildNumber -eq $TargetBuild) {
